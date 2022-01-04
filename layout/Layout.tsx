0ef4cd47654fedfdfb855e325641881c238dc5e8
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2022-01-04 15:19:20 -0500

    build: update to next-multilingual 0.9.1

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,10 +1,12 @@
+import type { ReactNode } from 'react';
 import Head from 'next-multilingual/head';
-import type { ReactElement, ReactNode } from 'react';
-import LanguagePicker from '@/components/LanguagePicker';
-import Footer from '@/components/Footer';
-import styles from './Layout.module.css';
 import Link from 'next-multilingual/link';
 import { useMessages } from 'next-multilingual/messages';
+
+import Footer from '@/components/Footer';
+import LanguagePicker from '@/components/LanguagePicker';
+
+import styles from './Layout.module.css';
 
 type LayoutProps = {
   /** The title of the page. */
@@ -17,7 +19,7 @@ type LayoutProps = {
  *
  * @param title - The title of the page.
  */
-export default function Layout({ title, children }: LayoutProps): ReactElement {
+export default function Layout({ title, children }: LayoutProps): JSX.Element {
   const messages = useMessages();
   return (
     <>
@@ -25,7 +27,7 @@ export default function Layout({ title, children }: LayoutProps): ReactElement {
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
-      <header className={styles.header}>
+      <header id="header" className={styles.header}>
         <div>
           {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
           <a href="/">{messages.format('header')}</a>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,40 @@
-const { getConfig } = require('next-multilingual/config');
+// const { getConfig } = require('next-multilingual/config');
 
-const config = getConfig('exampleApp', ['en-US', 'fr-CA'], {
-  poweredByHeader: false
-});
+// const config = getConfig('exampleApp', ['en-US', 'fr-CA'], {
+//   poweredByHeader: false,
+//   // debug: true,
+// });
 
-module.exports = config;
+// console.dir(config, { depth: null });
+
+// module.exports = config;
+
+const { Config } = require('next-multilingual/config');
+
+const config = new Config('exampleApp', ['en-US', 'fr-CA']);
+
+module.exports = {
+  i18n: {
+    locales: config.getUrlLocalePrefixes(),
+    defaultLocale: config.getDefaultUrlLocalePrefix(),
+    localeDetection: false,
+  },
+  poweredByHeader: false,
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.resolve.alias['next-multilingual/link$'] = require.resolve(
+        'next-multilingual/link/ssr'
+      );
+      config.resolve.alias['next-multilingual/head$'] = require.resolve(
+        'next-multilingual/head/ssr'
+      );
+    }
+    return config;
+  },
+  async rewrites() {
+    return config.getRewrites();
+  },
+  async redirects() {
+    return config.getRedirects();
+  },
+};
